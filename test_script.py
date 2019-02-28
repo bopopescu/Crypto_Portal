e17d786dd98a82eb04b66bdebb3be08f1b947166
Khendle <1490320@students.wits.ac.za>
2019-03-01 01:53:55 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,1 +1,11 @@
 
+import unittest
+from Experiment import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        self.assertEqual(greeter.message, 'Hello world!')
+
+if __name__ == '__main__':
+    unittest.main()
