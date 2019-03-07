9e5414df369117e540fed68d2f9e8a6c51c5c6b7
Khendle <1490320@students.wits.ac.za>
2019-03-08 01:41:22 +0200

    Update main_file.py
    Worked on the sign up page need to fix the the conflicting password entry boxes

--- main_file.py
+++ main_file.py
@@ -1,14 +1,18 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
-#import Tkinter as tk     # python 2
-#import tkFont as tkfont  # python 2
+from extra_functions import pass_word_check
+
 
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Crypto-Portal")
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        self.labels_font = tkfont.Font(family='Helvetica', size=14, weight="bold", slant="italic")
+        self.padding = ""
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -45,9 +49,9 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="This is the start page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="SignIn",
+        button1 = tk.Button(self, text="Sign in", padx=10, pady=10,
                             command=lambda: controller.show_frame("SignIn"))
-        button2 = tk.Button(self, text="SignUp",
+        button2 = tk.Button(self, text="Sign up", padx=10, pady=10,
                             command=lambda: controller.show_frame("SignUp"))
         button1.pack()
         button2.pack()
@@ -59,10 +63,12 @@ class SignIn(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="This is the sign-in page ", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        label.grid(column=0, row=0, sticky="N")
+
+
+        back = tk.Button(self, text="Back",
                            command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        back.grid(column=0,row=1, sticky=("S","E"))
 
 
 class SignUp(tk.Frame):
@@ -71,10 +77,65 @@ class SignUp(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="This is the sign up page.", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+
+        #label.pack(side="top", fill="x", padx=10, pady=10)
+        label.grid(column=0, row=0, sticky="N")
+
+        """Adding the labels required fields for the sign up page"""
+        tk.Label(self,text="Name",font=controller.labels_font).grid(column=0, row=1, sticky=("N","W"))
+        tk.Label(self, text="Last Name", font=controller.labels_font).grid(column=0, row=2, sticky=("N", "W"))
+        tk.Label(self, text="Phone", font=controller.labels_font).grid(column=0, row=3, sticky=("N", "W"))
+        tk.Label(self, text="Email", font=controller.labels_font).grid(column=0, row=4, sticky=("N", "W"))
+        tk.Label(self, text="Password", font=controller.labels_font).grid(column=0, row=5, sticky=("N", "W"))
+        tk.Label(self, text="Re-enter \n password", font=controller.labels_font).grid(column=0, row=6 , rowspan=2,
+                                                                                      sticky=("N", "W"))
+
+        """Creating entry fields and variables which will store data from the entry fields"""
+        self.name = tk.StringVar()
+        name_ent = tk.Entry(self, textvariable=self.name)
+        name_ent.grid(column=0, row=1, sticky=("N","E"))
+
+        self.last_name = tk.StringVar()
+        last_name_ent = tk.Entry(self, textvariable=self.last_name)
+        last_name_ent.grid(column=0, row=2, sticky=("N", "E"))
+
+        self.phone = tk.StringVar()
+        phone_ent = tk.Entry(self, textvariable=self.phone)
+        phone_ent.grid(column=0, row=3, sticky=("N", "E"))
+
+        self.email = tk.StringVar()
+        email_ent = tk.Entry(self, textvariable=self.email)
+        email_ent.grid(column=0, row=4, sticky=("N", "E"))
+
+        self.password = tk.StringVar
+        password_ent = tk.Entry(self, show="*", textvariable=self.password)
+        password_ent.grid(column=0, row=5, sticky=("N", "E"))
+
+        self.password2 = tk.StringVar
+        password2_ent = tk.Entry(self, show="*", textvariable=self.password2)
+        password2_ent.grid(column=0, row=7, sticky=("N", "E"))
+
+        create_profile_btn = tk.Button(self,text="Create profile",command=self.click_create_profile)
+        create_profile_btn.grid(column=1, row=8, sticky=("S","E"))
+
+        self.test = tk.Label(self, text="Please complete your profile.")
+        self.test.grid(column=0, row=9, sticky=("E","W"))
+
+        back_btn = tk.Button(self, text="Back", command=lambda: controller.show_frame("StartPage"))
+        back_btn.grid(column=0, row=8, sticky=("S","W"))
+
+    def click_create_profile(self):
+        print(self.name.get())
+        print(self.last_name.get())
+        print(self.phone.get())
+        print(self.email.get())
+        print(self.password)
+        print(self.password2)
+        if pass_word_check(self.password, self.password2):
+            self.test.configure(text="You have successfully created a profile "+self.name.get())
+        else:
+            self.test.configure(text="You have failed to create a profile.")
+
 
 
 if __name__ == "__main__":
