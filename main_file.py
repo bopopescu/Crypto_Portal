a180d4f1e824caad84545d11d4577f02e4db91f9
matlalaReuben <48084216+matlalaReuben@users.noreply.github.com>
2019-04-17 16:15:40 +0200

    organising

--- main_file.py
+++ main_file.py
@@ -1,18 +1,23 @@
+
 import tkinter as tk
-import tkinter.font as tkfont
-import databases as database
-import extra_functions as extra_functions
-import api_functions
-import webbrowser
+import tkinter.font as tk_font
+import extra_functions
+
+from startup import *
+from signup import *
+from signin import *
+from mainpage import *
 from profile import *
+import api_functions
+
+
+#import database
+#import urllib.request, json
 
 class EntryWithPlaceholder():
     def __init__(self, entry, placeholder="PLACEHOLDER"):
         self.entry = entry
         self.placeholder = placeholder
-        # print("self.placeholder = ", self.placeholder)
-        # self.placeholder_color = color
-        # print("self.placeholder_color =", self.placeholder_color )
         self.default_fg_color = self.entry['fg']
 
         self.entry.bind("<FocusIn>", self.foc_in)
@@ -25,7 +30,6 @@ class EntryWithPlaceholder():
         self.entry.config(fg="gray")
 
     def foc_in(self, *args):
-        # print("foc_in")
         if self.entry['fg'] == "gray":
             self.entry.delete('0', 'end')
             self.entry['fg'] = self.default_fg_color
@@ -42,7 +46,7 @@ class SampleApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Crypto-Portal")
         # self.state("zoomed")
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.title_font = tk_font.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.padding = ""
 
         # the container is where we'll stack a bunch of frames
@@ -66,284 +70,19 @@ class SampleApp(tk.Tk):
 
         self.show_frame("StartPage")
 
+
+    def update(self, page_name):
+        self.frames[page_name].update()
+
+    #DISPLAY PAGE
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
+        self.update( page_name )
+        frame = self.frames[ page_name ]
         frame.tkraise()
 
-
-class SignIn(tk.Frame):
-    def __init__(self, parent, controller):
-        self.controller = controller
-        tk.Frame.__init__(self, parent)
-        # self.con.state("zoomed")
-        self.columnconfigure(0, weight=1)
-
-        canvas = tk.Canvas(self, bg="yellow green", bd=0)
-        canvas.create_text(650, 140, text="Crypto Portal", anchor=tk.CENTER, font="Times 70 bold", fill="green")
-        canvas.create_text(650, 140, text="Crypto Portal", anchor=tk.CENTER, font="Times 71 bold", fill="lime green")
-        canvas.grid(row=0, column=0, sticky="NEWS", columnspan=10, rowspan=10)
-
-        tk.Label(self).grid(row=0, column=0)
-        self.rowconfigure(0, weight=10)
-
-        self.default_font = font = tkfont.Font(family="Times", size=15)
-        self.default_bg = "yellow green"
-
-        self.username = tk.StringVar(self)
-        username_entry = tk.Entry(self, bg=self.default_bg, font=self.default_font, textvariable=self.username,
-                                  fg="green")
-        username_entry.grid(row=1, column=0, ipady=10, pady=2, ipadx=40)
-        self.rowconfigure(1, weight=1)
-        EntryWithPlaceholder(username_entry, placeholder="Username")
-
-        self.password = tk.StringVar(self)
-        password_entry = tk.Entry(self, bg=self.default_bg, font=self.default_font, show="*",
-                                  textvariable=self.password, fg="green")
-        password_entry.grid(row=2, column=0, ipady=10, pady=2, ipadx=40)
-        self.rowconfigure(2, weight=1)
-        EntryWithPlaceholder(password_entry, placeholder="Password")
-
-        self.messege = tk.StringVar()
-        tk.Label(self, text="", textvariable=self.messege).grid(row=3, column=0)
-
-        tk.Button(self, bg=self.default_bg, fg="blue", text="Login", font=self.default_font, bd=0,
-                  command=self.do_login).grid(row=4, column=0, sticky="N", ipady=6, pady=(2))
-        self.rowconfigure(3, weight=1)
-
-        tk.Button(self, text="Forgot password?", bg=self.default_bg, bd=0, fg="red", font=self.default_font,
-                  command=self.do_forgot_password).grid(row=5, column=0, sticky="N", ipadx=35, ipady=3)
-        self.rowconfigure(4, weight=1)
-        tk.Button(self, text="Back", font=self.default_font, fg="green", bg=self.default_bg, bd=3,
-                  command=lambda: self.controller.show_frame("StartPage")).grid(row=6, column=0, sticky="S", ipadx=15,
-                                                                                ipady=5, pady=(0, 100))
-        self.rowconfigure(5, weight=1)
-
-    def do_login(self):
-        result = extra_functions.login_check( self.username.get(), self.password.get() )
-        if result == "You have successfully logged in.":
-            # login
-            self.controller.show_frame("MainPage")
-        else:
-            self.messege.set( result )
-
-    def do_forgot_password(self):
-        print("do_forgot_password")
-        self.controller.show_frame("MainPage")
-
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        self.default_bg = "yellow green"
-
-        self.columnconfigure(0, weight=1)
-
-        canvas = tk.Canvas(self, bg=self.default_bg, bd=0)
-        canvas.create_text(650, 140, text="Crypto Portal", anchor=tk.CENTER, font="Times 70 bold", fill="green")
-        canvas.create_text(650, 140, text="Crypto Portal", anchor=tk.CENTER, font="Times 71 bold", fill="lime green")
-        canvas.grid(row=0, column=0, sticky="NEWS", columnspan=10, rowspan=10)
-
-        self.rowconfigure(0, weight=3)
-
-        button_font = tkfont.Font(family="Times", size=13)
-
-        button1 = tk.Button(self, text="Sign in", bg=self.default_bg, fg="green", font=button_font,
-                            command=lambda: controller.show_frame("SignIn"))
-        button1.grid(row=1, column=0, ipadx=30, ipady=8)
-        self.rowconfigure(1, weight=1)
-
-        button2 = tk.Button(self, text="Sign up", fg="green", bg=self.default_bg, font=button_font,
-                            command=lambda: controller.show_frame("SignUp"))
-        button2.grid(row=2, column=0, ipadx=30, ipady=10, sticky="N", pady=(0, 100))
-        self.rowconfigure(2, weight=1)
-
-
-class SignUp(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        self.default_font = font = tkfont.Font(family="Times", size=15)
-        self.default_bg = "yellow green"
-        self.default_fg = "green"
-
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        canvas = tk.Canvas(self, bg=self.default_bg, bd=0)
-        canvas.create_text(650, 110, text="Crypto Portal", anchor=tk.CENTER, font="Times 70 bold", fill="green")
-        canvas.create_text(650, 110, text="Crypto Portal", anchor=tk.CENTER, font="Times 71 bold", fill="lime green")
-        canvas.grid(row=0, column=0, sticky="NEWS", columnspan=20, rowspan=20)
-        self.rowconfigure(0, weight=7)
-
-        """Adding the labels required fields for the sign up page"""
-        tk.Label(self, text="Username", font=self.default_font, fg=self.default_fg, bg=self.default_bg).grid(column=0,row=1,sticky="E")
-        tk.Label(self, text="Phone", font=self.default_font, fg=self.default_fg, bg=self.default_bg).grid(column=0, row=2,sticky="E")
-        tk.Label(self, text="Email", font=self.default_font, fg=self.default_fg, bg=self.default_bg).grid(column=0,row=3, sticky="E")
-        tk.Label(self, text="Password", font=self.default_font, fg=self.default_fg, bg=self.default_bg).grid(column=0,row=4, sticky="E")
-        tk.Label(self, text="Re-enter password", font=self.default_font, fg=self.default_fg, bg=self.default_bg).grid(column=0, row=5, sticky="E")
-
-        for i in range(1, 6):
-            self.rowconfigure(i, weight=1)
-
-        """Creating entry fields and variables which will store data from the entry fields"""
-
-        self.username = tk.StringVar()
-        name_ent = tk.Entry(self, textvariable=self.username, bg=self.default_bg, font=self.default_font)
-        name_ent.grid(column=1, row=1, sticky="W")
-
-        self.phone = tk.StringVar()
-        phone_ent = tk.Entry(self, textvariable=self.phone, bg=self.default_bg, font=self.default_font)
-        phone_ent.grid(column=1, row=2, sticky="W")
-
-        self.email = tk.StringVar()
-        email_ent = tk.Entry(self, textvariable=self.email, bg=self.default_bg, font=self.default_font)
-        email_ent.grid(column=1, row=3, sticky="W")
-
-        self.password = tk.StringVar()
-        password_ent = tk.Entry(self, show="*", textvariable=self.password, bg=self.default_bg, font=self.default_font)
-        password_ent.grid(column=1, row=4, sticky="W")
-
-        self.password2 = tk.StringVar()
-        password2_ent = tk.Entry(self, show="*", textvariable=self.password2, bg=self.default_bg, font=self.default_font)
-        password2_ent.grid(column=1, row=5, sticky="W", pady=(13, 0))
-
-        create_profile_btn = tk.Button(self, bg=self.default_bg, fg="green", text="Create profile",command=self.click_create_profile, font=self.default_font)
-        create_profile_btn.grid(column=0, columnspan=2, row=6, pady=(15, 0), ipadx=10, ipady=4)
-
-        back_btn = tk.Button(self, bg=self.default_bg, fg="green", text="Back",command=lambda: controller.show_frame("StartPage"), font=self.default_font)
-        back_btn.grid(column=1, row=7)
-
-        self.test = tk.Label(self, text="Please complete your profile.", bg=self.default_bg, fg="green")
-        self.test.grid(column=1, row=8, pady=(0, 70))
-
-    # Button for the create profile button if successful it will take you to the MainPage else a error message will pop up
-    def click_create_profile(self):
-        username = self.username.get()
-        phone = self.phone.get()
-        email = self.email.get()
-        password = self.password.get()
-        password2 = self.password2.get()
-        return_message = extra_functions.details_check(username, email, phone, password, password2)
-        if return_message == "Account created!":
-            #add_user_to_database(self,username, hash_password, random_string, email, phone)
-            #database.create_user(username=username, password=password, email=email, phone=phone)
-            database.create_user(self, username, email, phone, password)
-            self.controller.show_frame("MainPage")
-        else:
-            self.test.configure(text=return_message)
-
-
-# A page to allow changing of profile details, it will be reached via the profile settings button in the main page.
-"""
-class Profile(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        #Fix the font to show the page properly
-        label_ = tk.Label(self, text="Profile Settings")
-        label_.grid(sticky="W")
-
-        #database.get_username()
-        tk.Label(self, text="Username").grid(column="2", columnspan="3", row="1")
-
-        #get user info from database(emai;, phone and coin data)
-        tk.Label(self, text="Email").grid(column="0", columnspan="3", row="3")
-        tk.Label(self, text="Phone").grid(column="0", columnspan="3", row="5")
-
-
-        #Text entry to take in changes to the above details
-        #We create functions to post the data to the database
-        new_email_ = tk.StringVar()
-        new_email = tk.Entry(self, textvariable=new_email_)
-        new_email.grid(column="3", columnspan="3", row="3")
-
-        new_phone_ = tk.StringVar()
-        new_phone = tk.Entry(self, textvariable=new_phone_)
-        new_phone.grid(column="3", columnspan="3", row="5")
-
-        back_btn = tk.Button(self, text="Back", command=lambda: controller.show_frame("MainPage"))
-        back_btn.grid(column="3", columnspan="3", row="7")
-
-        #creating a function to post the changes to the database and take us back to the main page
-        #Apply_changes = tk.Button(self, text="Page Two",command=)
-
-"""
-# The page where our wallets portfolios will be created
-class MainPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        self.options_frame = tk.Frame(self)
-        self.options_frame.grid(column="0", columnspan="2", row="0")
-
-        profile = tk.Button(self.options_frame, text="Profile Setteings",
-                            command=lambda: controller.show_frame("Profile"))
-        profile.grid(column="0", columnspan="2", row="0")
-
-        log_out = tk.Button(self.options_frame, text="Logout", command=lambda: self.controller.show_frame("StartPage"))
-        log_out.grid(column="3", row="0")
-
-        # create a Frame for the Text and Scrollbar
-        self.txt_frame = tk.LabelFrame(self, text=" Article Information", width=350, height=200)
-        self.txt_frame.grid(column="0", columnspan="2", row="5")
-        # ensure a consistent GUI size
-        self.txt_frame.grid_propagate(False)
-        # implement stretchability
-        self.txt_frame.grid_rowconfigure(0, weight=1)
-        self.txt_frame.grid_columnconfigure(0, weight=1)
-
-        # create a Text widget
-        self.txt = tk.Text(self.txt_frame, borderwidth=3, relief="sunken")
-        self.txt.config(font=("consolas", 12), undo=True, wrap='word')
-        self.txt.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
-
-        # create a Scrollbar and associate it with txt
-        self.scrollb = tk.Scrollbar(self.txt_frame, command=self.txt.yview)
-        self.scrollb.grid(row=0, column=1, sticky='nsew')
-        self.txt['yscrollcommand'] = self.scrollb.set
-
-        # creating a frame for the article name and description
-        self.hline_frame = tk.Frame(self)
-        self.hline_frame.grid(column=0, columnspan=2, row=2, padx=5, pady=5)
-        self.hline_frame.rowconfigure(0, weight=1)
-        self.hline_frame.columnconfigure(0, weight=1)
-
-        tkvar = tk.StringVar(self)
-        choices = api_functions.tittle_source()
-        tkvar.set(choices[0])
-
-        self.pop_up_menu = tk.OptionMenu(self.hline_frame, tkvar, *choices)
-        tk.Label(self.hline_frame, text="Choose an article").grid(row=1, column=1)
-        self.pop_up_menu.grid(row=2, column=1)
-        d = api_functions.url_source()
-
-        def change_dropdown(*args):
-            i = choices.index(tkvar.get())
-            a = api_functions.articles_source()
-            b = api_functions.description_source()
-            c = api_functions.author_source()
-
-            self.txt.config()
-            self.txt.delete(1.0, tk.END)
-            self.txt.insert('1.0', 'Source: ' + a[i] + '\n\nTitle: ' + choices[i] + '\n\nAuthor: ' + c[
-                i] + '\n\nDescription: ' + b[i])
-            return i
-
-        def open_web():
-            webbrowser.open(d[choices.index(tkvar.get())])
-
-        self.link_btn = tk.Button(self.hline_frame, text="Read Full article", command=open_web)
-        self.link_btn.grid(row=4, column=0, columnspan=2)
-        tkvar.trace('w', change_dropdown)
 
 
 if __name__ == "__main__":
     app = SampleApp()
+
     app.mainloop()
