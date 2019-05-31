a605050f49d490cd4269e184e535c5b67e71442c
matlalaReuben <48084216+matlalaReuben@users.noreply.github.com>
2019-05-31 12:37:13 +0200

    signin now stores username

--- signin.py
+++ signin.py
@@ -3,18 +3,22 @@ import tkinter as tk
 import extra_functions
 #from main_file import EntryWithPlaceholder
 import database
+import tkinter.font as fonts
 
 
 
 class SignIn(tk.Frame):
     def __init__(self, parent, controller):
+
+        self.current_username = None
+
         self.controller = controller
         tk.Frame.__init__(self, parent)
         # self.con.state("zoomed")
         self.columnconfigure(0, weight=1)
 
-        #DECLARING DEFAULT COLOR AND FONT
-        self.default_font = font = tk.font.Font(family="Times", size=15)
+        # DECLARING DEFAULT COLOR AND FONT
+        self.default_font = font = fonts.Font(family="Times", size=15)
         self.default_bg = "yellow green"
 
         canvas = tk.Canvas(self, bg="yellow green", bd=0)
@@ -45,7 +49,7 @@ class SignIn(tk.Frame):
         self.error_messege = tk.Label(self, text="", fg="red",bg=self.default_bg , font=self.default_font ,textvariable=self.messege).grid(row=3, column=0)
 
         tk.Button(self, bg=self.default_bg, fg="blue", text="Login", font=self.default_font, bd=0,
-                  command=self.do_login).grid(row=4, column=0, sticky="N", ipady=6, pady=(2))
+                  command=self.do_login).grid(row=4, column=0, sticky="N", ipady=6, pady=2)
         self.rowconfigure(3, weight=1)
 
         tk.Button(self, text="Forgot password?", bg=self.default_bg, bd=0, fg="red", font=self.default_font,
@@ -57,29 +61,33 @@ class SignIn(tk.Frame):
         self.rowconfigure(5, weight=1)
 
     def do_login(self):
-        result = extra_functions.login_check( self.username.get(), self.password.get() )
+        result = extra_functions.login_check( self.username.get(), self.password.get())
         if result == "You have successfully logged in.":
             self.controller.show_frame("MainPage")
-            database.set_current_username( self.username.get() )
-            #self.clear_entries()
+            self.current_username = self.username.get()
+            self.controller.set_current_username(self.username.get())
+            # database.set_current_username(self.username.get())
+
         else:
-            self.messege.set( result )
+            self.messege.set(result)
 
     def do_forgot_password(self):
         print("do_forgot_password")
         self.controller.show_frame("MainPage")
 
-    def put_placeholder(self, entry , placeholder):
+    def put_placeholder(self, entry, placeholder):
         entry.delete(0, 'end')
         entry.insert(0, placeholder)
         entry.config(fg="gray")
 
-    def update(self):
-        self.put_placeholder( entry=self.username_entry, placeholder="Username" )
-        self.put_placeholder( entry=self.password_entry, placeholder="Password" )
+    def update_page(self):
+        print("updating signin")
+        self.put_placeholder(entry=self.username_entry, placeholder="Username")
+        self.put_placeholder(entry=self.password_entry, placeholder="Password")
         self.messege.set("")
 
-
+    # def get_current_username(self):
+    #     return self.current_username
 
 class EntryWithPlaceholder():
     def __init__(self, entry, placeholder="PLACEHOLDER"):
